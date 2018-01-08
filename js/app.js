6538fb229a7c948c78805760b31a5c8ee6f34790
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-01-07 22:53:24 -0500

    testing started. adds random array item when clicked

--- js/app.js
+++ js/app.js
@@ -2,8 +2,26 @@ $(() => {
 
 console.log('still working with jquery');
 
+const $search = $('#gen-btn');
+const $results = $('.results');
+let $result = null;
 
+const data = [
+  'first item',
+  'second item',
+  'third item',
+  'fourth item'
+];
 
+const randomItem = () => {
+  let $result = data[Math.floor(Math.random()*data.length)];
+  $results.append($result);
+};
+
+$search.on('click', (event) => {
+  randomItem();
+  console.log('working');
+});
 
 
 
