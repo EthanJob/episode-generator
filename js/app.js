d925c1c4fdd5e633a76ff2e2e0e450af5f739080
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-01-08 14:37:35 -0500

    created favicon placeholder

--- js/app.js
+++ js/app.js
@@ -5,12 +5,20 @@ const $search = $('#gen-btn');
 const $results = $('.results');
 
 // DATABASE
-const data = (dataBase);
+// const data = (dataBase);
+
+const data = (fullData);
 
 // FUNCTIONS
 const $clear = () => {
   $('.new').remove();
 };
+
+// const randomItem = () => {
+//   $results.append( $('<div>').addClass('new') );
+//   let $result = data[Math.floor(Math.random()*data.length)];
+//   $('.new').append($result);
+// };
 
 const randomItem = () => {
   $results.append( $('<div>').addClass('new') );
--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ const dataBase = [
   'episode 5',
   'episode 6'
 ];
+
+const fullData = [
+  {
+    Season: 1,
+    Episodes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  },
+  {
+    Season: 2,
+    Episodes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  }
+];
