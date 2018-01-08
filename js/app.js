446f439b1956d106070b9fd9181cae6bedda8b50
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-01-08 14:50:25 -0500

    edited button css

--- js/app.js
+++ js/app.js
@@ -5,13 +5,19 @@ const $search = $('#gen-btn');
 const $results = $('.results');
 
 // DATABASE
-// const data = (dataBase);
+const data = (dataBase);
 
-const data = (fullData);
+// const data = (fullData);
 
 // FUNCTIONS
 const $clear = () => {
   $('.new').remove();
+};
+
+const randomItem = () => {
+  $results.append( $('<div>').addClass('new') );
+  let $result = data[Math.floor(Math.random()*data.length)];
+  $('.new').append($result);
 };
 
 // const randomItem = () => {
@@ -19,12 +25,6 @@ const $clear = () => {
 //   let $result = data[Math.floor(Math.random()*data.length)];
 //   $('.new').append($result);
 // };
-
-const randomItem = () => {
-  $results.append( $('<div>').addClass('new') );
-  let $result = data[Math.floor(Math.random()*data.length)];
-  $('.new').append($result);
-};
 
 // ACTIONS
 $search.on('click', (event) => {
